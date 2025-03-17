dd2dda276f089f6c35827b0c888c836b2c9f5da0
Baetato <diana021015@gmail.com>
2025-03-17 16:40:45 +0900

    feat: dfs구현

---
+++ Baesuyeon_zone/week01/dfs.py
@@ -0,0 +1,22 @@
+# 그래프를 인접 리스트로 구현한 예시
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+# DFS 함수 정의
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set() #방문한 노드에 대해 집합을 사용하여 추적
+    visited.add(start)
+    print(start, end=' ')
+    for next_node in graph[start]:
+        if next_node not in visited:
+            dfs(graph, next_node, visited)
+
+# 시작할 노드를 선택하여 DFS를 호출한다.
+dfs(graph, 'A')
